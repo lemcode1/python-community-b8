cd990659b677e244d50160432c93a998d7a13d35
kasiy <kasi.yedumati@gmail.com>
2023-06-21 09:11:45 +0530

    datastructures_part2

--- datastructures_part1.py
+++ datastructures_part1.py
@@ -1,7 +1,58 @@
-b1=bytes([10,20,30,40])
-print(type(b1))
-b2=bytes([0,3,4,55,254])
-print(b2[3])
-print(b1[-1])
-b2[3] = 123
-print(b2)
+# b1=bytes([10,20,30,40])
+# print(type(b1))
+# b2=bytes([0,3,4,55,254])
+# print(b2[3])
+# print(b1[-1])
+# b2[3] = 123
+# print(b2)
+
+# bytearray
+# a= bytearray([10,20,30,40])
+#
+# print(type(a))
+# # b=bytearray([12,256,300,40])
+# print(a[2])
+# for i in a:
+#     print(i)
+# a[0] = 200
+# # print(a)
+# for i in a:
+#     print(i)
+
+# person_name="Prabhakar"
+# print(type(person_name))
+
+# list
+# student_names=["naresh","venky","sai","kasi","sai"]
+# print(student_names[0:4])
+# print(student_names)
+# print(type(student_names))
+# student_data=["kasi",1234,98.3,True,10+20j]
+# print(type(student_data))
+# print(student_data[1])
+# print(student_data[-1])
+# print(student_names[0:3])
+# student_names[0]="durga"
+# print(student_names)
+# student_names.append("suresh")
+# print(student_names)
+# print(len(student_names))
+# print(student_data)
+# a="ram"
+# student_names.append(a)
+# print(student_names)
+# student_names.remove("kasi")
+# print(student_names)
+# student_names.remove("sai")
+# print(student_names)
+
+# student_rollno=['117x1A04B2','117x1A04B1','117x1A04B0']
+# student_rollno.append('117x1A04B0')
+# print(student_rollno)
+
+# tuple data type
+# t1=(10,20,30,40)
+# print(type(t1))
+# print(t1[2])
+# student_data=("kasi",1234,98.3) # list, tuple i am maintainig insertion order
+# print(student_data[1])
---
+++ datastructures_part2.py
@@ -0,0 +1,39 @@
+# s1={10,20,30,100}
+# #    0  1  2  3
+# print(type(s1))
+# s2=set()
+# print(type(s2))
+# print(s1)
+# # print(s1[0])
+# s1.update([200,300,5000])
+# s1.add(9000)
+# s1.add("durga")
+# s1.add("durga")
+# print(s1)
+# s2={"kasi","ramesh","ram","kasi"}
+# print(s2)
+# l1=[10,20,20,30,40]
+# print(l1)
+# s3=set(l1) # converting list to set
+# print(s3)
+# s2.remove("ramesh")
+# print(s2)
+
+#vowels=('a','e','i','o','u','A','E','I','O','U')
+
+# # frozenset
+# vowels=('a','e','i','o','u','A','E','I','O','U')
+# print(vowels)
+# vowels2= frozenset(vowels)
+# print(vowels2)
+# print(type(vowels2))
+
+
+# dict datatype
+student_data = {'1234':'Jr Ntr','1235':'Ram charan'}
+# a.add(100)
+# print(type(a))
+# student_data = {''}
+student_data = {'1234':'Jr Ntr','1235':'Ram charan', '1234':'kasi'}
+print(type(student_data))
+print(student_data)
