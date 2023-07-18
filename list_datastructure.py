ceb75900597fd0f06c43fca94407fd58cce00f18
kasiy <kasi.yedumati@gmail.com>
2023-07-18 17:02:51 +0530

    list datastructure

--- list_datastructure.py
+++ list_datastructure.py
@@ -93,11 +93,92 @@ even_list = []
 odd_list = []
 output = []
 # [[10, 20, 30, 40, 6, 8, 2, 4, 6], [5,7,1,3,5]]
-for i in list:
-    if i%2==0:
-        even_list.append(i)
-    else:
-        odd_list.append(i)
-output.extend(even_list)
-output.extend(odd_list)
-print(output)
+# for i in list:
+#     if i%2==0:
+#         even_list.append(i)
+#     else:
+#         odd_list.append(i)
+# output.extend(even_list)
+# output.extend(odd_list)
+# print(output)
+# l1=['A','B','C']
+# l2 =['X','Y','Z']
+# # l1.append(l2)
+# l1.extend(l2)
+# print(l1)
+# l2.insert(1, 'A')
+# l2.insert(10, 'B')
+# l2.insert(-9, 'C')
+# l2.insert(-9, [1,2,3])
+# l2.insert(2, 'B')
+# print(l2)
+# l2.remove('A')
+# rm = l2.remove('B')
+# print(rm)
+# # l2.remove('ABC')
+# print(l2)
+# rm2= l2.pop()
+# print(rm2)
+# print(l2)
+# rm3= l2.pop()
+# print(rm3)
+# print(l2)
+# # l3=[]
+# # rm4= l3.pop()
+# rm4= l2.pop(0)
+# print(l2)
+# print(rm4)
+
+# Ordering elements of List
+# a=[20,3,14,5]
+# a.reverse()
+# print(a)
+# a.sort() # ascending/alphabatical
+# print(a)
+# # a.append('ABC')
+# # print(a)
+# a.sort(reverse=True)  # descending order
+# print(a)
+# a.sort(reverse=False)  # Ascending order
+# print(a)
+# # a = []
+# a.clear()
+# print(a)
+# x=[10,20,30,40]
+# y=x.copy()
+# # y=x
+# print(x)
+# print(y)
+# print(id(x))
+# print(id(y))
+# x[0]=300
+# print(x)
+# print(y)
+# print(x+y)
+# print(x*3)
+
+list1 = [2,3,4,5,7,8,9,1234,45,67,8]
+# even_numbers = []
+# for i in list1:
+#     if i%2 == 0:
+#         even_numbers.append(i)
+# print(even_numbers)
+# even_numbers = [i for i in list1 if i%2==0]
+# print(even_numbers)
+# # square_list = []
+# # for i in even_numbers:
+# #     square_list.append(i**2)
+# square_list = [i**2 for i in list1 if i%2==0]
+# print(square_list)
+
+
+
+s=input("Enter a number: ")
+arm_strong=0
+for i in s:
+     arm_strong=arm_strong+int(i)**3
+print(arm_strong)
+if s==str(arm_strong):
+     print("{} is armstrong number".format(s))
+else:
+     print("{} is not armstrong number".format(s))
